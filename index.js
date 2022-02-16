81bee16ddfc09f3bff3409a642a230dbf55bbcce
Monike Freitas <monike.ftsousa@gmail.com>
2022-02-16 00:27:13 -0400

    add on not found frame

--- index.js
+++ index.js
@@ -2,32 +2,50 @@ const app = Vue.createApp({
   el: "#app",
   data() {
     return {
-      day: 0,
+      day: 1,
+      isDayNull: false,
     };
+  },
+  methods: {
+    changeDay() {
+      fetch(`day-${this.day}/index.html`)
+        .then(async function (response) {
+          if (!response.ok) throw Error
+          return response.text();
+        })
+        .then((html) => {
+          this.isDayNull = false;
+          document.getElementById("frame").innerHTML = html;
+          document.head.getElementsByTagName(
+            "link"
+          )[0].href = `day-${this.day}/style.css`;
+          document.body.removeChild(document.getElementById("day-script"));
+          var newScript = document.createElement("script");
+          newScript.id = "day-script";
+          newScript.src = `day-${this.day}/index.js`;
+          document.body.appendChild(newScript);
+        })
+        .catch( (err) =>{
+          console.log("Failed to fetch page: ", err);
+          this.isDayNull = true;
+          console.log(this.isDayNull)
+        });
+    },
+    nextDay() {
+      this.day += 1;
+    },
+    previousDay() {
+      if (this.day > 1) {
+        this.day -= 1;
+      }
+    },
   },
   watch: {
     day() {
-      document.head.getElementsByTagName(
-        "link"
-      )[0].href = `day-${this.day}/style.css`;
-
-      fetch(`day-${this.day}/index.html`)
-        .then(function (response) {
-          // When the page is loaded convert it to text
-          return response.text();
-        })
-        .then(function (html) {
-          document.getElementById("frame").innerHTML = html;
-        })
-        .catch(function (err) {
-          console.log("Failed to fetch page: ", err);
-        });
-
-      document.body.removeChild(document.getElementById("day-script"));
-      var newScript = document.createElement("script");
-      newScript.id = "day-script";
-      newScript.src = `day-${this.day}/index.js`;
-      document.body.appendChild(newScript);
+      this.changeDay();
     },
+  },
+  mounted() {
+    this.changeDay();
   },
 });
