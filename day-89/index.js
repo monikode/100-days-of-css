a74aa68aee45e33465f3f8acde7f0540a7b5b231
Monike Freitas <monike.ftsousa@gmail.com>
2022-02-17 23:31:44 -0400

    day 89

---
+++ day-89/index.js
@@ -0,0 +1,9 @@
+console.log("KJNCSDJBSK");
+console.log($(".frame"));
+
+for (let i = 0; i < 10; i++) {
+	for (let j = 0; j < 10; j++) {
+		$(".frame")[0].innerHTML +=
+			'<div class="section"><div></div><div></div></div>';
+	}
+}
