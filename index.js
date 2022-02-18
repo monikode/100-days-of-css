39746ad77f9068c63aa40da3c39b7f3a3bf3b632
Monike Freitas <monike.ftsousa@gmail.com>
2022-02-18 18:11:56 -0400

    frame animation

--- index.js
+++ index.js
@@ -8,28 +8,42 @@ const app = Vue.createApp({
   },
   methods: {
     changeDay() {
-      fetch(`day-${this.day}/index.html`)
-        .then(  (response) =>{
-          if (!response.ok) throw Error;
-          else this.isDayNull = false;
-          return response.text();
-        })
-        .then((html) => {
-          document.getElementById("frame").innerHTML = html;
-          document.head.getElementsByTagName(
-            "link"
-          )[0].href = `day-${this.day}/style.css`;
-          document.body.removeChild(document.getElementById("day-script"));
-          var newScript = document.createElement("script");
-          newScript.id = "day-script";
-          newScript.src = `day-${this.day}/index.js`;
-          document.body.appendChild(newScript);
-        })
-        .catch((err) => {
-          console.log("Failed to fetch page: ", err);
-          this.isDayNull = true;
-          console.log(this.isDayNull);
-        });
+      document.querySelector("#frame-container .box").style.width = "100%";
+      document.querySelector("#frame-container .box").style.left = "0";
+      document.querySelector("#frame-container .box").style.right = "auto";
+      console.log({ doc: document.querySelector("#frame-container .box") });
+
+      setTimeout(() => {
+        fetch(`day-${this.day}/index.html`)
+          .then((response) => {
+            if (!response.ok) throw Error;
+            else this.isDayNull = false;
+            return response.text();
+          })
+          .then((html) => {
+            document.getElementById("frame").innerHTML = html;
+            document.head.getElementsByTagName(
+              "link"
+            )[0].href = `day-${this.day}/style.css`;
+            document.body.removeChild(document.getElementById("day-script"));
+            var newScript = document.createElement("script");
+            newScript.id = "day-script";
+            newScript.src = `day-${this.day}/index.js`;
+            document.body.appendChild(newScript);
+
+            document.querySelector("#frame-container .box").style.width = "0%";
+            document.querySelector("#frame-container .box").style.right = "0";
+            document.querySelector("#frame-container .box").style.left = "auto";
+          })
+          .catch((err) => {
+            console.log("Failed to fetch page: ", err);
+            document.querySelector("#frame-container .box").style.width = "0%";
+            document.querySelector("#frame-container .box").style.right = "0";
+            document.querySelector("#frame-container .box").style.left = "auto";
+            this.isDayNull = true;
+            console.log(this.isDayNull);
+          });
+      }, 300);
     },
     nextDay() {
       if (this.day < 100) this.day += 1;
