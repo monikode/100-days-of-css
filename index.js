fcfc4f650614599d8bb9d0cd7c9f0b8f6c961560
Monike Freitas <monike.ftsousa@gmail.com>
2022-02-19 10:19:42 -0400

    day 80 and adjustments

--- index.js
+++ index.js
@@ -7,42 +7,65 @@ const app = Vue.createApp({
     };
   },
   methods: {
+    async findStyleSheet() {
+      await fetch(`day-${this.day}/style.css`)
+        .then((response) => {
+          if (!response.ok) throw Error;
+          else this.isDayNull = false;
+
+          document.head.getElementsByTagName(
+            "link"
+          )[0].href = `day-${this.day}/style.css`;
+          this.findHTML();
+        })
+        .catch((err) => {
+          document.head.getElementsByTagName("link")[0].href = ``;
+
+          console.log("Failed to fetch page: ", err);
+          this.isDayNull = true;
+        });
+    },
+    async findHTML() {
+      await fetch(`day-${this.day}/index.html`)
+        .then((response) => {
+          if (!response.ok) throw Error;
+          else this.isDayNull = false;
+          return response.text();
+        })
+        .then((html) => {
+          document.getElementById("frame").innerHTML = html;
+          this.findScript();
+        })
+        .catch((err) => {
+          this.isDayNull = true;
+        });
+    },
+    findScript() {
+      fetch(`day-${this.day}/index.js`)
+        .then((response) => {
+          if (!response.ok) throw Error;
+          if (document.getElementById("day-script"))
+            document.body.removeChild(document.getElementById("day-script"));
+          var newScript = document.createElement("script");
+          newScript.id = "day-script";
+          newScript.src = `day-${this.day}/index.js`;
+          document.body.appendChild(newScript);
+        })
+        .catch((err) => {
+          if (document.getElementById("day-script"))
+            document.body.removeChild(document.getElementById("day-script"));
+        });
+    },
     changeDay() {
       document.querySelector("#frame-container .box").style.width = "100%";
       document.querySelector("#frame-container .box").style.left = "0";
       document.querySelector("#frame-container .box").style.right = "auto";
-      console.log({ doc: document.querySelector("#frame-container .box") });
 
-      setTimeout(() => {
-        fetch(`day-${this.day}/index.html`)
-          .then((response) => {
-            if (!response.ok) throw Error;
-            else this.isDayNull = false;
-            return response.text();
-          })
-          .then((html) => {
-            document.getElementById("frame").innerHTML = html;
-            document.head.getElementsByTagName(
-              "link"
-            )[0].href = `day-${this.day}/style.css`;
-            document.body.removeChild(document.getElementById("day-script"));
-            var newScript = document.createElement("script");
-            newScript.id = "day-script";
-            newScript.src = `day-${this.day}/index.js`;
-            document.body.appendChild(newScript);
-
-            document.querySelector("#frame-container .box").style.width = "0%";
-            document.querySelector("#frame-container .box").style.right = "0";
-            document.querySelector("#frame-container .box").style.left = "auto";
-          })
-          .catch((err) => {
-            console.log("Failed to fetch page: ", err);
-            document.querySelector("#frame-container .box").style.width = "0%";
-            document.querySelector("#frame-container .box").style.right = "0";
-            document.querySelector("#frame-container .box").style.left = "auto";
-            this.isDayNull = true;
-            console.log(this.isDayNull);
-          });
+      setTimeout(async () => {
+        await this.findStyleSheet();
+        document.querySelector("#frame-container .box").style.width = "0%";
+        document.querySelector("#frame-container .box").style.right = "0";
+        document.querySelector("#frame-container .box").style.left = "auto";
       }, 300);
     },
     nextDay() {
